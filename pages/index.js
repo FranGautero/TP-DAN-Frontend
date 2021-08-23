7857f7112cceec2770a76f4fefa91b904dae3250
Francisco Gautero <francisco.gautero@gmail.com>
2021-08-23 14:25:40 -0300

    Keycloak funcando paaaa

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,18 @@
-import {
-  Button,
-  Flex,
-  Heading,
-  Input,
-  useColorModeValue,
-} from "@chakra-ui/react";
-
 import { useRouter } from "next/router";
-
-import Link from "next/link";
+import { useKeycloak } from "@react-keycloak/ssr";
 
 export default function LogIn() {
-  const formBackground = useColorModeValue("gray.100", "gray.700");
   const router = useRouter();
+  const { keycloak } = useKeycloak();
 
-  return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      overscrollY="hidden"
-    >
-      <Flex direction="column" background={formBackground} p={12} rounded={6}>
-        <Heading mb={6}>Iniciar Sesión</Heading>
-        <Input placeholder="e-mail" variant="filled" mb={3} type="email" />
-        <Input placeholder="pass" variant="filled" mb={6} type="password" />
+  // eslint-disable-next-line no-unused-expressions
+  keycloak.authenticated ? (
+    keycloak.hasRealmRole("cliente") ? (
+      <div onLoad={router.push("/home-comprador/MisPedidos")}></div>
+    ) : keycloak.hasRealmRole("empleado") ? (
+      <div onLoad={router.push("/home-vendedor/Pedidos")}></div>
+    ) : null
+  ) : null;
 
-        <Button onClick={() => router.push("/home-vendedor/Pedidos")} mb={6}>
-          Log in Vendedor
-        </Button>
-
-        <Button
-          onClick={() => router.push("/home-comprador/MisPedidos")}
-          mb={6}
-        >
-          Log in Comprador
-        </Button>
-        <Link href="/registro-usuario">
-          <a> Registrarse </a>
-        </Link>
-      </Flex>
-    </Flex>
-  );
+  return null;
 }
